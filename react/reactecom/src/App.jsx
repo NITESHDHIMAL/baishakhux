30f0ab673eea8bb807e51d13bd4e3636402d9adb
NITESHDHIMAL <niteshdhimal18@gmail.com>
2024-06-04 09:25:59 +0545

    logical and comparision operators

---
+++ react/reactecom/src/App.jsx
@@ -0,0 +1,117 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  // let a = 3;
+  // let b = 2;
+
+  // arithmetic operator 
+  // let c = a + b;
+  // let c = a - b;
+  // let c = a * b;
+  // let c = a / b;
+  // let c = a % b;
+
+  // let c = a ** b ;
+
+
+  // assignment operator 
+  //  a += b
+  //  a = a + b
+  //  a -= b
+
+  // comparision operator 
+  // == equal value 
+  // === equal value and data type 
+
+  // != not equal 
+  // !== not equal and data type 
+
+  // > greater than
+  // < less than 
+  // >= greater than or equal
+  {/* <= less than or equal   */ }
+
+  // console.log(c)
+
+
+  // if (condition) {
+  //   statement 
+  // }
+
+  //  if (condition) {
+  //   statement 
+  // } else {
+  //   staement 
+  // }
+
+
+  // if (condition) {
+  //   statement
+  // } else if {
+  //   statement
+  // } else if {
+  //   statement
+  // } else if {
+  //   statement
+  // } else {
+  //   statement 
+  // }
+
+
+  // if ( a == b) {
+  // if ( a === b) {
+  // if ( a != b) {
+  // if ( a !== b) {
+  // if ( a > b) {
+  // if ( a < b) {
+  // if ( a >= b) {
+  // if ( a <= b) {
+  //   console.log("True")
+  // } else {
+  //   console.log("False")
+  // }
+
+
+
+  // if ( a == b && a > b){
+  // if ( a == b || a > b){
+  //   console.log("True")
+  // } else{
+  //   console.log("False")
+  // }
+
+  // age => 13 - 19 => Teen 
+  // age => 20 - 25 => Adult 
+
+  // mark sheet 
+  // 0 - 35 => D 
+  // 40 -  
+
+  let marks = 10;
+  
+  if ( marks == 0 || marks < 16 ){
+    console.log("Need Improvement")
+  } else {
+    console.log("False")
+  }
+
+
+  return (
+      <>
+
+
+        <h2>Arithmetic Operator</h2>
+
+
+
+      </>
+
+    )
+}
+
+export default App
